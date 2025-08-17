6c2a4edfd1744242802a43ff3bfda7e23a77c845
utsav kamal <utsvkml@gmail.com>
2025-08-17 08:19:59 +0530

    project initialization

---
+++ src/test/java/com/sample/sales/tax/SalesTaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sales.tax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
